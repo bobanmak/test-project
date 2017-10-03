f8777008a7f6611be536906b5cf7bfe8a9117b72
DESKTOP-24BJE5K\Kapak <bobanmak@aol.de>
2017-10-03 15:03:34 +0200

    edit3

--- api/controllers/NotesController.js
+++ api/controllers/NotesController.js
@@ -40,6 +40,37 @@ module.exports = {
             res.redirect("/notes/list");
         });
         return false;   
-    }
+    },
+    edit: function(req,res){
+        Notes.findOne({id:req.params.id}).exec(function(err,notes){
+            if(err){
+                res.send(500, {error:"DB Error"});
+            }
+
+            res.view("edit",{notes:notes});
+        });
+    },
+    update: function(req,res){
+        var title = req.body.title;
+        var body = req.body.body;
+
+        Notes.create({id: req.params.id},{title:title, body:body}).exec(function(err){
+            if(err){
+                res.send(500, {error: "DB Error"});
+            }
+
+            res.redirect("/notes/list");
+        });
+        return false;
+    },
+    view: function(req,res){
+        Notes.findOne({id:req.params.id}).exec(function(err,notes){
+            if(err){
+                res.send(500, {error:"DB Error"});
+            }
+
+            res.view("view",{notes:notes});
+        });
+    },
 };
 
